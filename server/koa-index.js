83b5581745a32d663331fb5e26035d74f6949a12
qwertyuiop6 <w771783057@gmail.com>
2020-09-20 02:18:14 +0800

    add cors

--- server/koa-index.js
+++ server/koa-index.js
@@ -2,6 +2,7 @@ const Koa = require("koa");
 const Static = require("koa-static");
 const bodyParser = require("koa-bodyparser");
 const myRouter = require("./router");
+const cors = require("@koa/cors");
 // const session = require('koa-session');
 // const sessionConf = require('./config/sessionConf')
 
@@ -10,36 +11,46 @@ const app = new Koa({
   proxyIpHeader: "X-Real-IP",
   // maxIpsCount: 1,
 });
-app.proxy = true;
 
-async function preDo(ctx, next) {
-  const localhost = ["localhost", "127.0.0.1", "::1"];
-  const frontDevDomains = ["129.211.173.197", "todo.wtfk.world"];
+// async function preDo(ctx, next) {
+//   const localhost = ["localhost", "127.0.0.1", "::1"];
+//   const frontDevDomains = ["129.211.173.197", "todo.wtfk.world"];
 
-  let ip;
-  if (ctx.ip.startsWith("::ff")) {
-    ip = ctx.ip.split(":").pop();
-  } else {
-    ip = ctx.ip;
-  }
-  console.log(
-    "Find req from %s, method: %s , target: %s",
-    ip,
-    ctx.method,
-    ctx.host + ctx.path
-  );
+//   let ip;
+//   if (ctx.ip.startsWith("::ff")) {
+//     ip = ctx.ip.split(":").pop();
+//   } else {
+//     ip = ctx.ip;
+//   }
+//   console.log(
+//     "Find req from %s, method: %s , target: %s",
+//     ip,
+//     ctx.method,
+//     ctx.host + ctx.path
+//   );
 
-  if (localhost.includes(ip) || frontDevDomains.includes(ip)) {
-    ctx.set("Access-Control-Allow-Origin", "*");
-    ctx.set("Access-Control-Allow-Credentials", true);
-  }
-  await next();
-}
+//   if (localhost.includes(ip) || frontDevDomains.includes(ip)) {
+//     ctx.set("Access-Control-Allow-Origin", "*");
+//     ctx.set("Access-Control-Allow-Credentials", true);
+//   }
+//   await next();
+// }
 
 // app.keys = sessionConf.appKeys;
 
 app
-  .use(preDo)
+  // .use(preDo)
+  .use(
+    cors({
+      origin: function(ctx) {
+        return ctx.origin;
+      },
+      credentials: true,
+      allowMethods: ["GET", "POST", "DELETE", "PATCH"],
+      allowHeaders: ["authorization", "Content-Type"],
+      maxAge: 86400, //24-hours
+    })
+  )
   .use(Static(`${__dirname}/../dist`))
   .use(bodyParser())
   // .use(session(sessionConf.config, app))
