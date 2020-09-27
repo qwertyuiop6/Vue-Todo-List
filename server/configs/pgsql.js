9f025ab97795b48d1d130a5766a59df93b5e15b3
qwertyuiop6 <super771783057@foxmail.com>
2020-09-27 10:58:24 +0800

    compose config

--- server/configs/pgsql.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  host: "",
-  port: 5432,
-  database: "",
-  user: "",
-  password: "",
-};
--- server/configs/redis.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  port: 6379,
-  host: "",
-  family: 4,
-  db: "0",
-  auth: "",
-};
