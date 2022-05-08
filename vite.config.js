5d00ccad523620022ea2b793c61e754658a4bd6f
qwertyuiop6 <super771783057@foxmail.com>
2022-05-09 01:53:38 +0800

    vue3!

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,33 @@
 import { defineConfig } from "vite";
-import { createVuePlugin } from "vite-plugin-vue2";
+// import { createVuePlugin } from "vite-plugin-vue2";
+import vue from "@vitejs/plugin-vue";
+// import vue from "@vue/compat";
 import WindiCSS from "vite-plugin-windicss";
 import path from "path";
 
 export default defineConfig({
-  plugins: [createVuePlugin(), WindiCSS({ preflight: false })],
   resolve: {
     alias: [
       {
         find: "@",
         replacement: path.resolve(__dirname, "src"),
       },
+      // { vue: "@vue/compat" },
     ],
   },
+  plugins: [
+    // createVuePlugin(),
+    vue({
+      // template: {
+      //   compilerOptions: {
+      //     compatConfig: {
+      //       MODE: 2,
+      //     },
+      //   },
+      // },
+    }),
+    WindiCSS({ preflight: false }),
+  ],
   server: {
     port: 8080,
     proxy: {
